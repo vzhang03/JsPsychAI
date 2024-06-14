ead8bb3900383d27c934d4d6cbf271771da5343f
vzhang03 <vzhang@vassar.edu>
2024-06-14 11:06:39 -0400

    Got dynamic rendering of html to work, need to adjust to keep screen requirements

---
+++ src/AIWindow.tsx
@@ -0,0 +1,10 @@
+
+
+export default function AIWindow(){
+
+  return (
+    <div className="AIWindow">
+      <div>this is the ai window</div>
+    </div>
+  )
+}
---
+++ src/ExperimentWindow.tsx
@@ -0,0 +1,12 @@
+import ExperimentComponent from "./ExperimentComponent"
+
+export default function ExperimentWindow(){
+
+
+  return (
+    <div className='ExperimentWindow'>
+      {/* <div>this is the experiment</div> */}
+      <ExperimentComponent />
+    </ div>
+  )
+}
